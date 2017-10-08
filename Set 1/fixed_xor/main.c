1e02f439fe7e3adaf5b3803920301f821a9cfd75
Matthew Bobrowski <mbobrowski@mbobrowski.org>
2017-10-08 14:49:39 +1100

    Minor validation checks and code refactoring implemented.

--- Set 1/fixed_xor/main.c
+++ Set 1/fixed_xor/main.c
@@ -1,4 +1,10 @@
-/**
+/*
+ * This source file contains the functions that are responsible for producing
+ * the xor'd output value given two equal length hexdecimal encoded buffers.
+ * This code has been purely written in order to solve the challenges posted on
+ * the Cryptopals website. More information about the Fixed XOR challenge can be
+ * found here <http://cryptopals.com/sets/1/challenges/2>.
+ *
  * Author: Matthew Bobrowski <mbobrowski@mbobrowski.org, mbobrowski@au1.ibm.com>
  */
 
@@ -8,6 +14,9 @@
 #include <stdint.h>
 #include <inttypes.h>
 
+/*
+ * Utility function that converts a hexdecimal character into a nibble.
+ */
 static uint8_t
 char_to_nibble(char c)
 {
@@ -17,27 +26,70 @@ char_to_nibble(char c)
 	return 225;
 }
 
+/*
+ * Function that decodes a hexdecimal encoded string into a raw byte array.
+ */
 static uint8_t
-*hex_to_bytes(char *hex)
+*hex_to_bytes(char *hexadecimal, size_t *length)
 {
 	size_t i;
 	uint8_t *p, *bytes;
-	size_t length = strlen(hex) / 2;
+	*length = strlen(hexadecimal) / 2;
 
-	bytes  = malloc(length + 1);
+	bytes  = malloc(*length + 1);
 
-	for (i = 0, p = (uint8_t *) hex; i < length; i++) {
+	if (bytes == NULL)
+		return NULL;
+
+	for (i = 0, p = (uint8_t *) hexadecimal; i < *length; i++) {
 		bytes[i] = (char_to_nibble(*p) << 4) | char_to_nibble(*(p + 1));
 		p += 2;
 	}
 
-	bytes[length + 1] = '\0';
+	bytes[*length] = '\0';
 
 	return bytes;
 }
 
+/*
+ * Function that is responsible for caculating the xor value of two equal length
+ * buffers.
+ */
+static uint8_t
+*xor(char **input)
+{
+	size_t length, a_len, b_len;
+	uint8_t *xor, *a, *b;
+
+	a = hex_to_bytes(input[1], &a_len);
+	b = hex_to_bytes(input[2], &b_len);
+
+	if ((a == NULL) || (b == NULL))
+		return NULL;
+
+	length = ((a_len + b_len) / 2);
+	xor = malloc(length + 1);
+
+	if (xor == NULL)
+		return NULL;
+
+	for (size_t i = 0; i < length; i++) {
+		xor[i] = a[i] ^ b[i];
+	}
+
+	free(a);
+	free(b);
+
+	xor[length] = '\0';
+
+	return xor;
+}
+
+/*
+ * Print byte array in hexadecimal format.
+ */
 static void
-print(uint8_t *buffer)
+print_buffer(uint8_t *buffer)
 {
 	if (buffer == NULL) return;
 
@@ -47,53 +99,48 @@ print(uint8_t *buffer)
 	printf("\n");
 }
 
-static uint8_t
-*xor(char **input)
-{
-	size_t length, a_len, b_len;
-	uint8_t *xor, *a, *b;
-
-	a = hex_to_bytes(input[1]);
-	b = hex_to_bytes(input[2]);
-
-	size_t a_length = strlen(input[1]) / 2;
-	size_t b_length = strlen(input[2]) / 2;
-
-	length = ((a_length + b_length) / 2);
-
-	xor = malloc(length + 1);
-
-	for (size_t i = 0; i < length; i++) {
-		xor[i] = a[i] ^ b[i];
-	}
-
-	xor[length] = '\0';
-
-	free(a);
-	free(b);
-
-	return xor;
-}
-
 int
 main(int argc, char **argv)
 {
 	uint8_t *result = NULL;
 
+	// check whether correct number of command line arguments passed to the
+	// program
 	if (argc != 3) {
 		printf("Invalid number of arguments passed to the program. " \
 			"Terminating program.\n");
 		exit(EXIT_FAILURE);
 	}
 
+	// calculate length of buffers provided to program
+	size_t a_len = strlen(argv[1]);
+	size_t b_len = strlen(argv[2]);
+
+	// determine whether the hexadecimal encoded buffers passed to the program are of equal length
+	if (a_len != b_len) {
+		printf("Buffers provided as arguments to the program are not" \
+			" of equal length.\nTerminating program.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	// determine whether the arguments passed to the program are
+	// hexadecimal encoded strings
+	if ((a_len % 2 != 0) || (b_len % 2 != 0)) {
+		printf("Buffers provided to the program don't appear to "  \
+			"hexadecimal encoded. Please ensure that they're " \
+			"valid.\n");
+		exit(EXIT_FAILURE);
+	}
+
 	result = xor(argv);
 
-	if (result == NULL)
+	if (result == NULL) {
+		printf("Error producing fixed xor valued string.\n");
 		exit(EXIT_FAILURE);
+	}
 
-	print(result);
+	print_buffer(result);
 
 	free(result);
-
 	exit(EXIT_SUCCESS);
 }
